731d97719048ccc865ab6e7b66b09edb898dc61a
LarsHolzer <108394052+LarsHolzer@users.noreply.github.com>
2022-12-18 11:24:24 +0100

    Add play-buttons, winner choose, display results

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+let buttons = document.querySelectorAll(".button");   
+let playerChoice;
+let computerChoice;
+let playerScore = 0;
+let computerScore = 0;
+
+
 // Get random choice 
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
@@ -5,11 +12,41 @@ function getComputerChoice() {
     return choice;
 }
 
-// Get player input and convert it to lowercase
-function getUserInput() {
-    let playerInput = window.prompt("Choose between 'Rock','Paper' and 'Scissors'");
-    playerInput = playerInput.toLowerCase();
-    return playerInput;
+
+//Display winner
+function winnerDisplay(winner) {
+    let resultContainer = document.getElementById("results");
+    let para = document.createElement("p");
+    para.textContent = winner;
+
+    resultContainer.appendChild(para);
+}
+
+// Declare a winner
+function declareWinner() {
+    if(playerScore === 5) {
+        let resultContainer = document.getElementById("results");
+        let para = document.createElement("p");
+        para.textContent = "You won the game!";
+
+        resultContainer.appendChild(para);
+    }
+    if (computerScore === 5) {
+        let resultContainer = document.getElementById("results");
+        let para = document.createElement("p");
+        para.textContent = "You lost the game!";
+
+        resultContainer.appendChild(para);
+    }
+}
+
+//update Score
+function updatePlayerScore() {
+    document.getElementById('player_score').innerText = playerScore
+}
+
+function updateComputerScore() {
+    document.getElementById('computer_score').innerText = computerScore
 }
 
 // Pick winner
@@ -17,25 +54,46 @@ function playRound(computerChoice, playerChoice) {
     if (computerChoice === playerChoice) {
         return "This is a draw."
     } else if (computerChoice === "rock" && playerChoice === "scissors") {
+        computerScore++;
+        updateComputerScore();
         return "You lose! Rock beats Scissors";
     } else if (computerChoice === "paper" && playerChoice === "scissors") {
+        playerScore++;
+        updatePlayerScore()
         return "You won! Scissors beats Paper";
     } else if (computerChoice === "paper" && playerChoice === "rock") {
+        computerScore++;
+        updateComputerScore();
         return "You lost! Paper beats Rock" ;
     } else if (computerChoice === "scissors" && playerChoice === "rock") {
+        playerScore++;
+        updatePlayerScore()
         return "You won! Rock beats Scissors" ;
     } else if (computerChoice === "rock" && playerChoice === "paper") {
+        playerScore++;
+        updatePlayerScore()
         return "You won! Paper beats Rock" ;
     } else if (computerChoice === "scissors" && playerChoice === "paper") {
+        computerScore++;
+        updateComputerScore();
         return "You lost! Scissors beats paper";
     }
 }
 
-function game() {
-    for (let i = 0; i < 5 ; i++) {
-        let computerChoice = getComputerChoice();
-        let playerChoice = getUserInput();
-        playRound(computerChoice, playerChoice)
-        console.log(playRound(computerChoice,playerChoice))
-    }
-}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      playerChoice = button.value.toLowerCase();
+        console.log(playerChoice);
+      let computerChoice = getComputerChoice()
+    
+     let winner = playRound(computerChoice, playerChoice);
+     winnerDisplay(winner)
+  
+      if (playerScore === 5 || computerScore === 5) {
+        declareWinner();
+      }
+    });
+  });
+
+  
